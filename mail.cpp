b5694d8afe3b38456784d690dcdfdcd04bedfdad
tsda04 <tsda04@mail.ru>
2023-12-13 01:30:56 +0400

    замена типов

--- mail.cpp
+++ mail.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-
+using T = double;
 class Vector {
 private:
-	int size;
-	int* coordinate;
+	//size_t - помещает максимально возможный размер массива на данной архитектуре (и при этом не переполняется как int)
+	size_t size;
+	T* coordinate;
 public:
 	Vector() {
 		size = 0;
 		coordinate[0] = 0;
 	}
 
-	Vector(int n, int value) {
+	Vector(size_t n, int value) {
 		size = n;
-		coordinate = new int[size];
-		for (int i = 0; i < size; ++i)
+		coordinate = new T[size];
+		for (size_t i = 0; i < size; ++i)
 			coordinate[i] = value;
 	}
 
@@ -22,7 +23,7 @@ public:
 	}
 	void print() const {
 		std::cout << "{ ";
-		for (int i = 0; i < size-1; ++i)
+		for (size_t i = 0; i < size-1; ++i)
 			std::cout << coordinate[i] << "; ";
 		std::cout << coordinate[size - 1] << " ";
 		std::cout << "}";
